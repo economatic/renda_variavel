565c8784f12e5025cb014bb3a581bb6901b0268b
Marcio Ventura <82892702+economatic@users.noreply.github.com>
2024-07-27 13:42:42 +0000

    Dashboard

--- app.py
+++ app.py
@@ -10,12 +10,15 @@ from streamlit_extras.grid import grid
 
 
 def build_sidebar():
-    st.image("Imagens/3.png")
+    st.image("Imagens/2.png")
     ticker_list = pd.read_csv("tickers_ibra.csv", index_col=0)
     tickers = st.multiselect(label="Selecione as Empresas", options=ticker_list, placeholder='Ativos')
     tickers = [t+".SA" for t in tickers]
     start_date = st.date_input("De", format="DD/MM/YYYY", value=datetime(2022,1,2))
     end_date = st.date_input("Até", format="DD/MM/YYYY", value="today")
+
+
+
     st.write('Developed by Marcio V.')
     if tickers:
         prices = yf.download(tickers, start=start_date, end=end_date)["Adj Close"]
@@ -36,6 +39,8 @@ def build_main(tickers, prices):
     vols = returns.std()*np.sqrt(252)
     rets = (norm_prices.iloc[-1] - 100) / 100
 
+
+
     mygrid = grid(5 ,5 ,5 ,5 ,5 , 5, vertical_align="top")
     for t in prices.columns:
         c = mygrid.container(border=True)
@@ -45,6 +50,8 @@ def build_main(tickers, prices):
             colA.image("Imagens/compras.png")
         elif t == "IBOV":
             colA.image("Imagens/brasilia.png")
+        elif t == "Markowitz":
+            colA.image("Imagens/tecnologia.png")
         else:
             colA.image(f'https://raw.githubusercontent.com/thefintz/icones-b3/main/icones/{t}.png', width=85)
         colB.metric(label="retorno", value=f"{rets[t]:.0%}")
@@ -87,7 +94,9 @@ st.set_page_config(layout="wide")
 with st.sidebar:
     tickers, prices = build_sidebar()
 
-st.title('Mesa RV -BX investimentos-- IS')
+st.title('Mesa RV -- BX investimentos -- IS')
 
 if tickers:
     build_main(tickers, prices)
+
+
